e7778899f84ad6c7eea8d541242d453b2679c4ca
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-10-06 19:59:26 -0400

    feat: enhance hash input handling to support URL format

--- index.ts
+++ index.ts
@@ -15,7 +15,14 @@ type User = {
 };
 
 // ask user for the hash they want to crack
-const hash = prompt("Enter the hash you want to crack:");
+let hash = prompt("Enter the hash you want to crack:");
+
+// if the entered hash is in the format *https://zkcrush.xyz/crush?hash=5d7c6c2f2db598424f0f75ae7fa41b7b8b97294afaa41c58f84be6ce5d036dcd&name=*
+// then get the hash
+
+if (hash && hash.includes("https://zkcrush.xyz/crush?hash=")) {
+  hash = hash.split("https://zkcrush.xyz/crush?hash=")[1].split("&name=")[0];
+}
 
 // find the hash and then the userid associated
 const user = RainbowTableDB.prepare(
