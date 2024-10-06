3455a95bb55fbfbed1c818cf6413bb6dfd872297
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-10-06 15:30:20 -0400

    feat: implement hash cracking functionality with user lookup

--- index.ts
+++ index.ts
@@ -1,1 +1,42 @@
-console.log("Hello via Bun!");
+import { Database } from "bun:sqlite";
+
+const RainbowTableDB = new Database("hashes.db");
+type RainbowTable = {
+  id: string;
+  hashName: string;
+  hashRealName: string;
+};
+const userDB = new Database("users.db");
+type User = {
+  id: string;
+  name: string | null;
+  real_name: string | null;
+  is_admin: boolean;
+};
+
+// ask user for the hash they want to crack
+const hash = prompt("Enter the hash you want to crack:");
+
+// find the hash and then the userid associated
+const user = RainbowTableDB.prepare(
+  "SELECT * FROM RainbowTable WHERE hashName = ? OR hashRealName = ?"
+).get(hash, hash) as RainbowTable;
+
+if (!user) {
+  console.log("Hash not found in the rainbow table");
+  process.exit(0);
+}
+
+// get the user from the user db
+const userFromDB = userDB
+  .prepare("SELECT * FROM users WHERE id = ?")
+  .get(user.id) as User;
+
+if (!userFromDB) {
+  console.log("User not found in the user db");
+  process.exit(0);
+}
+
+console.log(
+  `The user with the hash ${hash} is @${userFromDB.name} / ${userFromDB.real_name} with the id ${userFromDB.id}`
+);
